9b514ebf54be65d5c78ed47a48bf6aee125b6975
finally <sogomonyandv@datum-group.ru>
2021-11-06 18:17:35 +0300

    style setting in starting script

--- start.py
+++ start.py
@@ -1,10 +1,14 @@
 import sys
 from PyQt5 import QtWidgets
 
+from backend.config_handler import ConfigHandler
+from backend.file_handler import fileHandler
 from backend.main import FormWindow
 
 if __name__ == '__main__':
     application = QtWidgets.QApplication(sys.argv)
+    with fileHandler(ConfigHandler.main_qss_file, ConfigHandler.file_in_mode) as file_in:
+        application.setStyleSheet(file_in.read())
     window = FormWindow()
     window.show()
     sys.exit(application.exec_())
